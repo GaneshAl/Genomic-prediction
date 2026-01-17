9ab2e6f427e17e9c800771c2d073754830794393
GaneshAl <106396280+GaneshAl@users.noreply.github.com>
2026-01-18 04:53:56 +0800

    models_BayesB.R

--- models_BayesB.R
+++ models_BayesB.R
@@ -4,14 +4,25 @@ suppressPackageStartupMessages({
 
 predict_fold_BayesB <- function(y, M, train_idx, test_idx,
                                 nIter = 12000, burnIn = 4000, thin = 5) {
-  y_cv <- y
-  y_cv[test_idx] <- NA
-  
+
+  ytr <- y[train_idx]
+  Xtr <- M[train_idx, , drop = FALSE]
+  Xte <- M[test_idx,  , drop = FALSE]
+
   fit <- BGLR(
-    y = y_cv,
-    ETA = list(list(X = M, model = "BayesB")),
+    y = ytr,
+    ETA = list(list(X = Xtr, model = "BayesB")),
     nIter = nIter, burnIn = burnIn, thin = thin,
     verbose = FALSE
   )
-  as.numeric(fit$yHat[test_idx])
+
+  # BGLR usually stores marker effects here for marker models
+  b <- fit$ETA[[1]]$b
+  if (is.null(b)) {
+    stop("BayesB strict OOF: could not find marker effects fit$ETA[[1]]$b in BGLR output.")
+  }
+
+  mu <- as.numeric(fit$mu)
+  as.numeric(mu + Xte %*% b)
 }
+
